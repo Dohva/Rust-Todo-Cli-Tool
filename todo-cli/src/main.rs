28b41d45ce0e89e7a50548607797e628a463119a
Dohva <Rallethekid@gmail.com>
2026-05-04 22:07:52 +0200

    create source file if not exists

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use std::fs;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -16,7 +17,14 @@ enum Commands {
 }
 
 fn main() {
+    let source_file = "todos.txt";
     let cli = Cli::parse();
+
+    println!("checking if source file exists");
+    match fs::File::create_new(source_file) {
+        Ok(_file) => println!("File not found. Successfully created"),
+        Err(_error) => println!("File already exists"),
+    };
 
     match &cli.command {
         Some(Commands::Echo { content }) => {
