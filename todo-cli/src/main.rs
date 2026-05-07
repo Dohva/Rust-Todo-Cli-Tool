0a458bb41e4feb2ee86acee0c71544d6b412614a
Dohva <Rallethekid@gmail.com>
2026-05-07 21:24:59 +0200

    add 'add' command to create todo items

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -17,7 +17,37 @@ enum Commands {
 fn run(command: Option<Commands>, source_file_path: &str) -> Result<String, String> {
     match command {
         Some(Commands::Echo { content }) => return Ok(content),
-        Some(Commands::Add { content }) => return Err(String::from("not implemented")),
+        Some(Commands::Add { content }) => {
+            let file_result = fs::OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(&source_file_path);
+
+            let mut file: fs::File;
+            match file_result {
+                Ok(opened_file) => file = opened_file,
+                Err(error) => {
+                    return Err(format!(
+                        "Source file could not be opened due to error: {error}"
+                    ));
+                }
+            }
+
+            let content_to_write = format!("{content}\n");
+            let content_bytes = content_to_write.as_bytes();
+
+            match file.write(content_bytes) {
+                Ok(bytes_written) => {
+                    if bytes_written < content_bytes.len() {
+                        return Err(String::from(
+                            "Could not write full task bytes to source file",
+                        ));
+                    }
+                }
+                Err(_content) => return Err(String::from("Could not write to source file")),
+            }
+            return Ok(format!("Successfully created task: \"{content}\""));
+        }
         None => return Err(String::from("Command not recognized")),
     };
 }
@@ -34,32 +64,6 @@ fn main() {
 
     match run(cli.command, source_file) {
         Ok(content) => println!("{content}"),
-        Err(content) => eprintln!("Error prevented command completion: {content}")
-    }
-
-    return;
-
-    match &cli.command {
-        Some(Commands::Echo { content }) => {
-            println!("{content}");
-        }
-        Some(Commands::Add { content }) => {
-            let file_result = fs::OpenOptions::new()
-                .create(true)
-                .append(true)
-                .open(&source_file);
-
-            let mut file: fs::File;
-            match file_result {
-                Ok(opened_file) => file = opened_file,
-                Err(error) => {
-                    eprintln!("Error loading source file. Error: {error}");
-                    process::exit(1);
-                }
-            }
-            // TODO pack into separate method to allow smoother error handling, eg. returning error results
-            file.write(&content.as_bytes());
-        }
-        None => {}
+        Err(content) => eprintln!("Error prevented command completion: {content}"),
     }
 }
