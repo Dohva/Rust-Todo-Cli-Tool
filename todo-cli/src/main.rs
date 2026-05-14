112511e5f06f3db7de075d043c4ab704c84f9064
Dohva <rallethekid@gmail.com>
2026-05-14 13:50:03 +0200

    refactor method 'add' into separate function

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -14,40 +14,42 @@ enum Commands {
     Add { content: String },
 }
 
+fn add(content: &str, source_file_path: &str) -> Result<String, String> {
+    let file_result = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(source_file_path);
+
+    let mut file: fs::File;
+    match file_result {
+        Ok(opened_file) => file = opened_file,
+        Err(error) => {
+            return Err(format!(
+                "Source file could not be opened due to error: {error}"
+            ));
+        }
+    }
+
+    let content_to_write = format!("{content}\n");
+    let content_bytes = content_to_write.as_bytes();
+
+    match file.write(content_bytes) {
+        Ok(bytes_written) => {
+            if bytes_written < content_bytes.len() {
+                return Err(String::from(
+                    "Could not write full task bytes to source file",
+                ));
+            }
+        }
+        Err(_content) => return Err(String::from("Could not write to source file")),
+    }
+    Ok(format!("Successfully created task: \"{content}\""))
+}
+
 fn run(command: Option<Commands>, source_file_path: &str) -> Result<String, String> {
     match command {
         Some(Commands::Echo { content }) => Ok(content),
-        Some(Commands::Add { content }) => {
-            let file_result = fs::OpenOptions::new()
-                .create(true)
-                .append(true)
-                .open(source_file_path);
-
-            let mut file: fs::File;
-            match file_result {
-                Ok(opened_file) => file = opened_file,
-                Err(error) => {
-                    return Err(format!(
-                        "Source file could not be opened due to error: {error}"
-                    ));
-                }
-            }
-
-            let content_to_write = format!("{content}\n");
-            let content_bytes = content_to_write.as_bytes();
-
-            match file.write(content_bytes) {
-                Ok(bytes_written) => {
-                    if bytes_written < content_bytes.len() {
-                        return Err(String::from(
-                            "Could not write full task bytes to source file",
-                        ));
-                    }
-                }
-                Err(_content) => return Err(String::from("Could not write to source file")),
-            }
-            Ok(format!("Successfully created task: \"{content}\""))
-        }
+        Some(Commands::Add { content }) => add(&content, source_file_path),
         None => Err(String::from("Command not recognized")),
     }
 }
